e3e1ffa2892376794dbde04cd389451d5bba4b60
jreisner <johntreisner@gmail.com>
2018-03-27 10:35:27 -0500

    Update subsetting historical.R

--- Scripts/subsetting historical.R
+++ Scripts/subsetting historical.R
@@ -1,72 +1,41 @@
 library(data.table)
 library(tidyr)
 library(dplyr)
+# original data file has 184,903,890 rows
+skip_ind <- c(1, (1:7) * 23112986 + 1)
+skip_ind
+nr <- 23112986
 
-df2 <- fread("~/Documents/502 Group Project/train.csv", header = TRUE, nrows = 20000000)
-fwrite(df2, "~/Documents/502 Group Project/df2.csv")
-df3 <- fread("~/Documents/502 Group Project/train.csv", header = FALSE, nrows = 20000000, skip = 30000000)
-fwrite(df3, "~/Documents/502 Group Project/df3.csv")
-df4 <- fread("~/Documents/502 Group Project/train.csv", header = FALSE, nrows = 20000000, skip = 70000000)
-fwrite(df4, "~/Documents/502 Group Project/df4.csv")
-df5 <- fread("~/Documents/502 Group Project/train.csv", header = FALSE, nrows = 20000000, skip = 110000000)
-fwrite(df5, "~/Documents/502 Group Project/df5.csv")
-df6 <- fread("~/Documents/502 Group Project/train.csv", header = FALSE, nrows = 20000000, skip = 150000000)
-fwrite(df6, "~/Documents/502 Group Project/df6.csv")
+read_fun <- function(i) {
+  df <- fread("~/Documents/502 Group Project/train.csv", header = FALSE, 
+              skip = skip_ind[i],
+              nrows = nr)
+  set.seed(i)
+  df_ind <- sample(1:nr, 625000)
+  df <- df[df_ind,]
+}
+df1 <- read_fun(1)
+df2 <- read_fun(2)
+df3 <- read_fun(3)
+df4 <- read_fun(4)
+df5 <- read_fun(5)
+df6 <- read_fun(6)
+df7 <- read_fun(7)
+df8 <- read_fun(8)
 
+df <- bind_rows(df1, df2, df3, df4, df5, df6, df7, df8)
+names(df) <- c("ip", "app", "device", "os", "channel", "click_time", 
+               "attributed_time", "is_attributed")
 
-names(df2) <- c("ip", "app", "device", "os", "channel", "click_time", "attributed_time",
-               "is_attributed")
-set.seed(2)
-df2_ind <- sample(1:nrow(df2), 1000000)
-df2_sub <- df2[df2_ind,]
-rm(df2)
-
-names(df3) <- c("ip", "app", "device", "os", "channel", "click_time", "attributed_time",
-               "is_attributed")
-set.seed(3)
-df3_ind <- sample(1:nrow(df3), 1000000)
-df3_sub <- df3[df3_ind,]
-rm(df3)
-
-names(df4) <- c("ip", "app", "device", "os", "channel", "click_time", "attributed_time",
-               "is_attributed")
-set.seed(4)
-df4_ind <- sample(1:nrow(df4), 1000000)
-df4_sub <- df4[df4_ind,]
-rm(df4)
-
-names(df5) <- c("ip", "app", "device", "os", "channel", "click_time", "attributed_time",
-                "is_attributed")
-set.seed(5)
-df5_ind <- sample(1:nrow(df5), 1000000)
-df5_sub <- df5[df5_ind,]
-rm(df5)
-
-names(df6) <- c("ip", "app", "device", "os", "channel", "click_time", "attributed_time",
-                "is_attributed")
-set.seed(6)
-df6_ind <- sample(1:nrow(df6), 1000000)
-df6_sub <- df6[df6_ind,]
-rm(df6)
-
-# combine into one data frame
-df <- bind_rows(df2_sub, df3_sub, df4_sub, df5_sub, df6_sub)
-
-# clear environment of unnecessary objects
-rm(df2_sub)
-rm(df3_sub)
-rm(df4_sub)
-rm(df5_sub)
-rm(df6_sub)
-
-# only 0.26% of observations are fradulent
+# only 0.245% of observations are fradulent
 sum(df$is_attributed) / nrow(df) * 100
 
 # separating date and time. lubridate package can be used to further decompose
 df <- separate(df, click_time, sep = "\\s", into = c("date", "time"), remove = FALSE)
 
 # save as a csv
-fwrite(df, "historical.csv")
+fwrite(df, "historical2.csv")
+
 
 
 
